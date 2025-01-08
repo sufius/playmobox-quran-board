425b39cebe65b47e413eed7577b4ae0bf7c2d1a6
Sufian Abu-Rab <s.aburab@gmail.com>
2025-01-08 04:19:51 +0100

    kick out initial example stuff add surah files, style sheets, html and logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,67 @@
-import Image from "next/image";
+import surah from "./surat/wbw-surah-18.json";
 import styles from "./page.module.css";
+
+interface RowProps {
+  index: number;
+  verse: {
+    text_uthmani_transcribed: string,
+    text_uthmani_tajweed_parsed: string,
+    translations: {
+      "id": number,
+      "resource_id": number,
+      "text": string
+    }[],
+  }
+}
+
+const Row = ({index, verse}: RowProps) => {
+  return <>
+    <div className={`position-absolute btn-close translate-middle ${styles['button-' + index]}`}></div>
+    <hr className={`position-absolute ${styles['button-' + index + '-divider']}`}></hr>
+    <div className={`position-absolute translate-middle-y text-transcribed ${styles['button-' + index + '-transcribed']}`}>
+        &#xFD3E;{index}&#xFD3F;&nbsp; 
+        <span style={{whiteSpace: "nowrap"}}
+          dangerouslySetInnerHTML={{__html:verse.text_uthmani_transcribed }} 
+        ></span>
+    </div>
+    <div className={`position-absolute translate-middle-y text-translated ${styles['button-' + index + '-translated']}`}>
+        &#xFD3E;{index}&#xFD3F;&nbsp;
+        {verse.translations.find(item => item.resource_id == 27)?.text}
+    </div>
+    <div className={`position-absolute translate-middle-y noto-naskh-arabic-400 text-arabic ${styles['button-' + index + '-arabic']}`}>
+        &#xFD3F;{index}&#xFD3E;&nbsp;
+        <span style={{whiteSpace: "nowrap"}}
+          dangerouslySetInnerHTML={{__html:verse.text_uthmani_tajweed_parsed }} 
+        ></span>
+    </div>
+  </>;
+};
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <>
+        <h6 className="position-absolute pmb-text-primary surah-number">
+            s&#363;rah: 18
+        </h6>
+        <h6 className="position-absolute pmb-text-primary ayat-numbers">
+            ʾāyāt: 1-11 [110]
+        </h6>
+        <h6 className="position-absolute surah-name-transcribed text-transcribed">
+            S&#363;rat<span title='HamztWslA' className= 'HamztWslA'> A&#8205;</span>&#8205;l-Kahf
+        </h6>
+        <h6 className="position-absolute text-translated surah-name-translated text-transcribed">
+            Sure: die Höhle
+        </h6>
+        <h6 className="position-absolute surah-name-arabic noto-naskh-arabic-400 text-arabic">
+            سُورَة الكَهف
+        </h6>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <span className="position-absolute pmb-module-footprint border border-1 bg-gradient"></span> 
+        <span className="position-absolute pmb-module-usb-footprint border border-1 bg-gradient"></span> 
+
+        {Array.from({ length: 11 }).map((_, index) => (
+          <Row key={index} index={index + 1} verse={surah.verses[index]} />
+        ))}
+    </>
   );
 }
