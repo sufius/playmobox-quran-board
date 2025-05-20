fde575f210c846e21b34b6b02c22e54cfc348ee3
Sufian Abu-Rab <s.aburab@gmail.com>
2025-05-20 19:58:09 +0200

    Hide the Next.js dev overlay route badge

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: false,
-  devIndicators: undefined,
+  reactStrictMode: false
 };
 
 export default nextConfig;
